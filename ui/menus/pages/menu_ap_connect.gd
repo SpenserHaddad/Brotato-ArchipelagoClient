919a80db48cc0bdb2daf7b122496ecaed1b2df0f
Spenser Haddad <spenser.haddad@gmail.com>
2023-09-06 20:46:00 -0400

    Tell client to connect when connect button is pressed

--- ui/menus/pages/menu_ap_connect.gd
+++ ui/menus/pages/menu_ap_connect.gd
@@ -14,15 +14,17 @@ func init():
 func _ready():
 	pass
 
-func _input(_event):
-	if Input.is_key_pressed(KEY_ENTER):
-		_on_ConnectButton_pressed()
+#func _input(_event):
+#	if get_tree().current_scene.name == self.name && Input.is_key_pressed(KEY_ENTER):
+#		_on_ConnectButton_pressed()
 
 func _on_ConnectButton_pressed():
-	var log_message = "Would connect with Host='{host}', Player='{player}', Password='{password}'".format(
-		{"host": _host_edit.text, 
-		"player": _player_edit.text,
-		"password": _password_edit.text
-	})
-	ModLoaderLog.info(log_message, ArchipelagoModBase.MOD_NAME)
+	var mod_node: ArchipelagoModBase = get_node("/root/ModLoader/RampagingHippy-Archipelago")
+	var ap_client: ApClientService = mod_node.ap_client
+	var server_info = _host_edit.text.rsplit(":", false, 1)
+	var server = server_info[0]
+	var port = int(server_info[1])
+	var player = _player_edit.text
+	var password = _password_edit.text
+	ap_client.connect_to_multiworld(server, port, player, password)
 	emit_signal("connect_button_pressed")
