63aa1e1ba3bc60a32b313dbab44be57783c5c947
Spenser Haddad <spenser.haddad@gmail.com>
2023-09-16 18:30:02 -0400

    Use brotato client, and fix incorrect label for connect state

--- ui/menus/pages/menu_ap_connect.gd
+++ ui/menus/pages/menu_ap_connect.gd
@@ -4,11 +4,13 @@ signal connect_button_pressed
 
 
 onready var _connect_button: Button = $"VBoxContainer/ConnectButton"
+onready var _connect_status_label: Label = $"VBoxContainer/ConnectStatusLabel"
 onready var _host_edit: LineEdit = $"VBoxContainer/CenterContainer/GridContainer/HostEdit"
 onready var _player_edit: LineEdit = $"VBoxContainer/CenterContainer/GridContainer/PlayerEdit"
 onready var _password_edit: LineEdit = $"VBoxContainer/CenterContainer/GridContainer/PasswordEdit"
 
 onready var ap_client
+onready var brotato_client
 
 
 func init():
@@ -19,6 +21,7 @@ func _ready():
 	var mod_node: ArchipelagoModBase = get_node("/root/ModLoader/RampagingHippy-Archipelago")
 	ap_client = mod_node.ap_client
 	ap_client.connect("connection_state_changed", self, "_on_connection_state_changed")
+	brotato_client = mod_node.brotato_client
 	_on_connection_state_changed(ap_client.connection_state)
 
 #func _input(_event):
@@ -29,24 +32,24 @@ func _on_connection_state_changed(new_state):
 	match new_state:
 		0:
 			# Connecting
-			_connect_button.text = "Connecting"
+			_connect_status_label.text = "Connecting"
 		1:
 			# Open
 			_connect_button.text = "Disconnect"
+			_connect_status_label.text = "Connected"
 		2:
 			# Closing
-			_connect_button.text = "Disconnecting"
+			_connect_status_label.text = "Disconnecting"
 		3:
 			# Closed
 			_connect_button.text = "Connect"
+			_connect_status_label.text = "Disonnected"
 
 func _on_ConnectButton_pressed():
-	var mod_node: ArchipelagoModBase = get_node("/root/ModLoader/RampagingHippy-Archipelago")
-	var ap_client = mod_node.ap_client
 	var server_info = _host_edit.text.rsplit(":", false, 1)
 	var server = server_info[0]
 	var port = int(server_info[1])
-	var player = _player_edit.text
-	var password = _password_edit.text
-	ap_client.connect_to_multiworld(server, port, player, password)
+	brotato_client.player = _player_edit.text
+	brotato_client.password = _player_edit.text
+	ap_client.connect_to_multiworld(server, port)
 	emit_signal("connect_button_pressed")
