ffd6dc8741525c1c0c4fd69f3a88a28bb476618f
Spenser Haddad <spenser.haddad@gmail.com>
2024-03-29 17:31:01 -0400

    Make rule factories instead of LogicMixin for simplicity

--- apworld/brotato/Rules.py
+++ apworld/brotato/Rules.py
@@ -1,12 +1,31 @@
-from BaseClasses import CollectionState
+from BaseClasses import CollectionState, Item, ItemClassification
+from worlds.generic.Rules import CollectionRule
 
-from ..AutoWorld import LogicMixin
 from .Items import ItemName
 
 
-class BrotatoLogic(LogicMixin):
-    def _brotato_has_character(self: CollectionState, player: int, character: str) -> bool:
-        return self.has(character, player)
+def create_has_run_wins_rule(player: int, count: int) -> CollectionRule:
+    def has_wins(state: CollectionState) -> bool:
+        return state.has(ItemName.RUN_COMPLETE.value, player, count)
 
-    def _brotato_has_run_wins(self: CollectionState, player: int, count: int) -> bool:
-        return self.has(ItemName.RUN_COMPLETE.value, player, count=count)
+    return has_wins
+
+
+def create_has_character_rule(player: int, character: str) -> CollectionRule:
+    def char_region_access_rule(state: CollectionState):
+        return state.has(character, player)
+
+    return char_region_access_rule
+
+
+def legendary_loot_crate_item_rule(item: Item) -> bool:
+    """Prevent progression items from being placed at legendary loot crate drops.
+
+    This can cause some very slow/painful BKs if not set
+
+    TODO: Ideally we would make the locations EXCLUDED, but that causes fill problems.
+    """
+    return item.classification not in (
+        ItemClassification.progression,
+        ItemClassification.progression_skip_balancing,
+    )
