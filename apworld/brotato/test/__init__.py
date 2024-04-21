d9b69aac002de0120a4c2e31cd062a9b10c531ac
Spenser Haddad <spenser.haddad@gmail.com>
2024-04-20 20:52:08 -0400

    Make BrotatoTestBase._run more flexible by taking a dict instead of a data set

--- apworld/brotato/test/__init__.py
+++ apworld/brotato/test/__init__.py
@@ -3,7 +3,7 @@ from test.bases import WorldTestBase
 from typing import ClassVar
 
 from .. import BrotatoWorld
-from ._data_sets import TEST_DATA_SETS, BrotatoTestDataSet
+from ._data_sets import TEST_DATA_SETS
 
 
 class BrotatoTestBase(WorldTestBase):
@@ -14,13 +14,13 @@ class BrotatoTestBase(WorldTestBase):
     def _test_data_set_subtests(self):
         for test_data in TEST_DATA_SETS:
             with self.subTest(msg=test_data.test_name()):
-                self._run(test_data)
+                self._run(asdict(test_data.options))
                 yield test_data
 
-    def _run(self, test_data: BrotatoTestDataSet):
+    def _run(self, options: dict):
         """Setup the world using the options from the dataset.
 
         We make this distinct from setUp() so tests can call this from subTests when iterating overt TEST_DATA_SETS.
         """
-        self.options = {**asdict(test_data.options)}
+        self.options.update(options)
         self.world_setup()
