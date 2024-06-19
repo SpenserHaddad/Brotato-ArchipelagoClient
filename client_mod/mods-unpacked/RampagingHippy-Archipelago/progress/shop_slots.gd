27b44060ebc8897bb0b2635732dcad14a5546df5
Spenser Haddad <spenser.haddad@gmail.com>
2024-06-19 16:06:34 -0400

    Remove probably copy/pasted fields

--- client_mod/mods-unpacked/RampagingHippy-Archipelago/progress/shop_slots.gd
+++ client_mod/mods-unpacked/RampagingHippy-Archipelago/progress/shop_slots.gd
@@ -3,9 +3,6 @@ class_name ApShopSlotsProgress
 
 var num_starting_shop_slots: int
 var num_unlocked_shop_slots: int
-var wins_for_goal: int
-var num_wins: int = 0
-var characters_won_with: PoolStringArray = []
 
 func _init(ap_client, game_state).(ap_client, game_state):
 	# Need this for Godot to pass through to the base class
