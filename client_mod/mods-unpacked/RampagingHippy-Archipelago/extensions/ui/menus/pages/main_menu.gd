51530343b51de1069df4b388285138e8f3ebaef4
Spenser Haddad <spenser.haddad@gmail.com>
2024-06-30 18:30:34 -0400

    Whitespace cleanup

--- client_mod/mods-unpacked/RampagingHippy-Archipelago/extensions/ui/menus/pages/main_menu.gd
+++ client_mod/mods-unpacked/RampagingHippy-Archipelago/extensions/ui/menus/pages/main_menu.gd
@@ -2,8 +2,8 @@ extends "res://ui/menus/pages/main_menu.gd"
 
 onready var _archipelago_button
 onready var _ap_websocket_connection
-var _ap_icon_connected = preload("res://mods-unpacked/RampagingHippy-Archipelago/ap_button_icon_connected.png")
-var _ap_icon_disconnected = preload("res://mods-unpacked/RampagingHippy-Archipelago/ap_button_icon_disconnected.png")
+var _ap_icon_connected = preload ("res://mods-unpacked/RampagingHippy-Archipelago/ap_button_icon_connected.png")
+var _ap_icon_disconnected = preload ("res://mods-unpacked/RampagingHippy-Archipelago/ap_button_icon_disconnected.png")
 
 signal ap_connect_button_pressed
 
@@ -21,12 +21,13 @@ func _add_ap_button():
 	var parent_node_name = "HBoxContainer/ButtonsLeft"
 	var parent_node: BoxContainer = get_node(parent_node_name)
 
-	ModLoaderMod.append_node_in_scene(self,
+	ModLoaderMod.append_node_in_scene(
+		self,
 		"ArchipelagoButton",
 		parent_node_name,
 		"res://mods-unpacked/RampagingHippy-Archipelago/ui/menus/pages/archipelago_connect_button.tscn"
 	)
-	_archipelago_button = get_node(parent_node_name + "/ArchipelagoButton")
+	_archipelago_button = get_node(parent_node_name +"/ArchipelagoButton")
 	parent_node.move_child(_archipelago_button, 0)
 	_archipelago_button.connect("pressed", self, "_on_MainMenu_ap_connect_button_pressed")
 
