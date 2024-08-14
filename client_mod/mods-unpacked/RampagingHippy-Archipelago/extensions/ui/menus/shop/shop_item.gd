a2e405ab360e38621fb8f46bed0f56cca1a8a5ca
Spenser Haddad <spenser.haddad@gmail.com>
2024-08-13 23:11:05 -0400

    Ensure lock button status is correct after shop item is activated

--- client_mod/mods-unpacked/RampagingHippy-Archipelago/extensions/ui/menus/shop/shop_item.gd
+++ client_mod/mods-unpacked/RampagingHippy-Archipelago/extensions/ui/menus/shop/shop_item.gd
@@ -1,10 +1,21 @@
 extends "res://ui/menus/shop/shop_item.gd"
 
+var _ap_lock_button_enabled: bool = true
+
+func activate():
+	.activate()
+	
+	# Activate always enables the lock button, so we may have to set it back to it's AP
+	# value.
+	if not _ap_lock_button_enabled:
+		disable_lock_button()
 
 func enable_lock_button():
-	_lock_button.disabled = false
+	_lock_button.activate()
 	_lock_button.text = "MENU_LOCK"
+	_ap_lock_button_enabled = true
 
 func disable_lock_button():
-	_lock_button.disabled = true
+	_lock_button.disable()
 	_lock_button.text = "Need item"
+	_ap_lock_button_enabled = false
