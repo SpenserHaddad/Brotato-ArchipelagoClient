7c7ece0868b6c8167aab40c38903d6450edcea90
Spenser Haddad <spenser.haddad@gmail.com>
2024-10-27 14:56:24 -0400

    Fix AP shop lock buttons

--- client_mod/mods-unpacked/RampagingHippy-Archipelago/extensions/ui/menus/shop/shop_item.gd
+++ client_mod/mods-unpacked/RampagingHippy-Archipelago/extensions/ui/menus/shop/shop_item.gd
@@ -1,21 +1,19 @@
 extends "res://ui/menus/shop/shop_item.gd"
 
-var _ap_lock_button_enabled: bool = true
+var ap_lock_button_enabled: bool = true
 
-func activate():
-	.activate()
+func manage_lock_button_visibility() -> void:
+	.manage_lock_button_visibility()
+
+	# The game may disable the lock button using certain items, we need to make sure we
+	# don't override this.
+	if not ap_lock_button_enabled and not _lock_button.disabled:
+		_lock_button.disable()
+		_lock_button.text = "Need item"
+	else:
+		# Trust that the game has set the lock button properly, just set the text back
+		# in case we changed it before and received an lock button item from AP.
+		_lock_button.text = "MENU_LOCK"
+
 	
-	# Activate always enables the lock button, so we may have to set it back to it's AP
-	# value.
-	if not _ap_lock_button_enabled:
-		disable_lock_button()
-
-func enable_lock_button():
-	_lock_button.activate()
-	_lock_button.text = "MENU_LOCK"
-	_ap_lock_button_enabled = true
-
-func disable_lock_button():
-	_lock_button.disable()
-	_lock_button.text = "Need item"
-	_ap_lock_button_enabled = false
+		
