6973777b7cf69be3c19071983113454d299b8738
Spenser Haddad <spenser.haddad@gmail.com>
2025-02-11 22:15:03 -0500

    Make comments in data sets clearer

--- apworld/brotato/test/data_sets/shop_slots.py
+++ apworld/brotato/test/data_sets/shop_slots.py
@@ -24,42 +24,42 @@ class ShopSlotsTestCase:
 SHOP_SLOT_TEST_DATA_SETS: list[ShopSlotsTestCase] = []
 
 for num_shop_slots, num_lock_buttons in product(range(MAX_SHOP_SLOTS + 1), range(MAX_SHOP_SLOTS + 1)):
-    # all should always be 4
     SHOP_SLOT_TEST_DATA_SETS.append(
         ShopSlotsTestCase(
             num_starting_shop_slots=num_shop_slots,
             lock_button_mode=StartingShopLockButtonsMode(StartingShopLockButtonsMode.option_all),
             num_starting_lock_buttons=num_lock_buttons,
+            # option_all: Starting lock buttons should always be 4
             expected_num_starting_lock_buttons=MAX_SHOP_SLOTS,
         )
     )
 
-    # none should always be 0
     SHOP_SLOT_TEST_DATA_SETS.append(
         ShopSlotsTestCase(
             num_starting_shop_slots=num_shop_slots,
             lock_button_mode=StartingShopLockButtonsMode(StartingShopLockButtonsMode.option_none),
             num_starting_lock_buttons=num_lock_buttons,
+            # option_none: Starting lock buttons should always be 0
             expected_num_starting_lock_buttons=0,
         )
     )
 
-    # match_shop_slots should disregard num_lock_buttons
     SHOP_SLOT_TEST_DATA_SETS.append(
         ShopSlotsTestCase(
             num_starting_shop_slots=num_shop_slots,
             lock_button_mode=StartingShopLockButtonsMode(StartingShopLockButtonsMode.option_match_shop_slots),
             num_starting_lock_buttons=num_lock_buttons,
+            # option_match_shop_slots: Starting lock buttons should disregard num_lock_buttons
             expected_num_starting_lock_buttons=num_shop_slots,
         )
     )
 
-    # custom should ignore num_slots
     SHOP_SLOT_TEST_DATA_SETS.append(
         ShopSlotsTestCase(
             num_starting_shop_slots=num_shop_slots,
             lock_button_mode=StartingShopLockButtonsMode(StartingShopLockButtonsMode.option_custom),
             num_starting_lock_buttons=num_lock_buttons,
+            # option_custom: Starting lock buttons should match num_starting_lock_buttons
             expected_num_starting_lock_buttons=num_lock_buttons,
         )
     )
