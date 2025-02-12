ac12b0aa61c2bc81e4deb601c11b37b06a88642a
Spenser Haddad <spenser.haddad@gmail.com>
2025-02-11 22:49:18 -0500

    Add logs when getting starting shop slots and shop slot items (#70)
    Trying to gather more info to track down progressive shop lock issue.

--- client_mod/mods-unpacked/RampagingHippy-Archipelago/progress/shop_slots.gd
+++ client_mod/mods-unpacked/RampagingHippy-Archipelago/progress/shop_slots.gd
@@ -2,6 +2,8 @@
 extends "res://mods-unpacked/RampagingHippy-Archipelago/progress/_base.gd"
 class_name ApShopSlotsProgress
 
+const LOG_NAME = "RampagingHippy-Archipelago/progress/shop_slots"
+
 var num_starting_shop_slots: int
 var num_unlocked_shop_slots: int
 
@@ -12,7 +14,13 @@ func _init(ap_client, game_state).(ap_client, game_state):
 func on_item_received(item_name: String, _item):
 	if item_name == constants.SHOP_SLOT_ITEM_NAME:
 		num_unlocked_shop_slots += 1
+		ModLoaderLog.info("Shop slot received, count=%d" % num_unlocked_shop_slots, LOG_NAME)
 
 func on_connected_to_multiworld():
 	num_starting_shop_slots = _ap_client.slot_data["num_starting_shop_slots"]
 	num_unlocked_shop_slots = num_starting_shop_slots
+	ModLoaderLog.info(
+		"num_starting_shop_slots=%d, num_unlocked_shop_slots=%d" % 
+		[num_starting_shop_slots, num_unlocked_shop_slots], 
+		LOG_NAME
+	)
