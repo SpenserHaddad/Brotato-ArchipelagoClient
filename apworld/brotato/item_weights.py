c2c580649933a10bb8b40ea96da98e0a468c029c
Spenser Haddad <spenser.haddad@gmail.com>
2025-03-30 15:33:52 -0400

    Use option types for arguments to ensure type correctness

--- apworld/brotato/item_weights.py
+++ apworld/brotato/item_weights.py
@@ -1,42 +1,53 @@
 import itertools
 import math
+from collections import Counter
 from random import Random
 
+from Options import OptionError
+
+from . import options
 from .items import ItemName, item_name_groups
 
 
 def create_items_from_weights(
     num_items: int,
     random: Random,
-    common_item_weight: int,
-    uncommon_item_weight: int,
-    rare_item_weight: int,
-    legendary_item_weight: int,
-    common_upgrade_weight: int,
-    uncommon_upgrade_weight: int,
-    rare_upgrade_weight: int,
-    legendary_upgrade_weight: int,
-    gold_weight: int,
-    xp_weight: int,
-) -> list[ItemName]:
+    common_item_weight: options.CommonItemWeight,
+    uncommon_item_weight: options.UncommonItemWeight,
+    rare_item_weight: options.RareItemWeight,
+    legendary_item_weight: options.LegendaryItemWeight,
+    common_upgrade_weight: options.CommonUpgradeWeight,
+    uncommon_upgrade_weight: options.UncommonUpgradeWeight,
+    rare_upgrade_weight: options.RareUpgradeWeight,
+    legendary_upgrade_weight: options.LegendaryUpgradeWeight,
+    gold_weight: options.GoldWeight,
+    xp_weight: options.XpWeight,
+) -> dict[ItemName, int]:
     gold_items = [ItemName(g) for g in sorted(item_name_groups["Gold"])]
     xp_items = [ItemName(x) for x in sorted(item_name_groups["XP"])]
-    gold_item_weights = _create_weights_for_item_group(gold_weight, gold_items)
-    xp_item_weights = _create_weights_for_item_group(xp_weight, xp_items)
+    gold_item_weights = _create_weights_for_item_group(gold_weight.value, gold_items)
+    xp_item_weights = _create_weights_for_item_group(xp_weight.value, xp_items)
     item_name_to_weight: dict[ItemName, int] = {
-        ItemName.COMMON_ITEM: common_item_weight,
-        ItemName.UNCOMMON_ITEM: uncommon_item_weight,
-        ItemName.RARE_ITEM: rare_item_weight,
-        ItemName.LEGENDARY_ITEM: legendary_item_weight,
-        ItemName.COMMON_UPGRADE: common_upgrade_weight,
-        ItemName.UNCOMMON_UPGRADE: uncommon_upgrade_weight,
-        ItemName.RARE_UPGRADE: rare_upgrade_weight,
-        ItemName.LEGENDARY_UPGRADE: legendary_upgrade_weight,
+        ItemName.COMMON_ITEM: common_item_weight.value,
+        ItemName.UNCOMMON_ITEM: uncommon_item_weight.value,
+        ItemName.RARE_ITEM: rare_item_weight.value,
+        ItemName.LEGENDARY_ITEM: legendary_item_weight.value,
+        ItemName.COMMON_UPGRADE: common_upgrade_weight.value,
+        ItemName.UNCOMMON_UPGRADE: uncommon_upgrade_weight.value,
+        ItemName.RARE_UPGRADE: rare_upgrade_weight.value,
+        ItemName.LEGENDARY_UPGRADE: legendary_upgrade_weight.value,
         **gold_item_weights,
         **xp_item_weights,
     }
 
-    return random.choices(list(item_name_to_weight.keys()), weights=list(item_name_to_weight.values()), k=num_items)
+    try:
+        chosen_items = random.choices(
+            list(item_name_to_weight.keys()), weights=list(item_name_to_weight.values()), k=num_items
+        )
+    except ValueError as ve:
+        # The Python docs state that random.choices raises a ValueError if all weights are zero.
+        raise OptionError("At least one item weight must be >0") from ve
+    return Counter(chosen_items)
 
 
 def _create_weights_for_item_group(weight: int, group: list[ItemName]) -> dict[ItemName, int]:
