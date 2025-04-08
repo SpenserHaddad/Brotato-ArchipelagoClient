dd62218339e3324314a6b554b76e159d2ecc5436
Spenser Haddad <spenser.haddad@gmail.com>
2025-04-07 20:28:10 -0400

    Fix item wave values to properly distribute them over waves 1-20
    The attempt to bias the items towards lower waves just meant that the game wouldn't give any items higher wave numbers if the number of items was less than 20.
    
    It's probably not great for balance, but there's bigger issues in that regard (this barely does anything in-game), and it leads to more natural results. For example, there will always be an item of wave 20 now, and the waves will actually be spaced along `[1, 20]` using a cheap `linspace` implementation.

--- apworld/brotato/test/test_waves.py
+++ apworld/brotato/test/test_waves.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+from ..constants import ItemRarity
+from ..items import ItemName
 from ..options import WavesPerCheck
 from ..waves import get_wave_for_each_item, get_waves_with_checks
 
@@ -41,4 +43,92 @@ class TestWavesWithChecks(TestCase):
 
 class TestGetWaveForEachItem(TestCase):
     def test_get_wave_for_each_item(self):
-        pass
+        """Check some common values for the number of wave items to check they all yield the correct results.
+
+        For simplicity, we only run one invocation using a dict with the four item tiers, which essentially gives us
+        four tests in one, plus another testing the output structure is correct.
+
+        We don't test any values that don't fit evenly into the number of waves here to make it easy to check exact
+        results.
+        """
+        # Test multiple values using the same dict for simplicity
+        item_counts: dict[ItemName, int] = {
+            ItemName.COMMON_ITEM: 40,
+            ItemName.UNCOMMON_ITEM: 20,
+            ItemName.RARE_ITEM: 10,
+            ItemName.LEGENDARY_ITEM: 0,
+        }
+
+        expected_wave_per_item: dict[int, list[int]] = {
+            # 2 items at each wave
+            ItemRarity.COMMON.value: [
+                1,
+                1,
+                2,
+                2,
+                3,
+                3,
+                4,
+                4,
+                5,
+                5,
+                6,
+                6,
+                7,
+                7,
+                8,
+                8,
+                9,
+                9,
+                10,
+                10,
+                11,
+                11,
+                12,
+                12,
+                13,
+                13,
+                14,
+                14,
+                15,
+                15,
+                16,
+                16,
+                17,
+                17,
+                18,
+                18,
+                19,
+                19,
+                20,
+                20,
+            ],
+            # Bias towards earlier rounds if the count doesn't evenly divide by 20
+            ItemRarity.UNCOMMON.value: [
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10,
+                11,
+                12,
+                13,
+                14,
+                15,
+                16,
+                17,
+                18,
+                19,
+                20,
+            ],
+            ItemRarity.RARE.value: [2, 4, 6, 8, 10, 12, 14, 16, 18, 20],
+            ItemRarity.LEGENDARY.value: [],
+        }
+
+        wave_per_item = get_wave_for_each_item(item_counts)
+        self.assertDictEqual(wave_per_item, expected_wave_per_item)
