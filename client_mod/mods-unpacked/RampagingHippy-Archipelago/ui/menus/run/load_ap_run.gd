547b701a10c712d86048506892ebeb0ccd9e0083
Spenser Haddad <spenser.haddad@gmail.com>
2025-07-01 22:19:14 -0400

    Implement resume functionality

--- client_mod/mods-unpacked/RampagingHippy-Archipelago/ui/menus/run/load_ap_run.gd
+++ client_mod/mods-unpacked/RampagingHippy-Archipelago/ui/menus/run/load_ap_run.gd
@@ -21,11 +21,11 @@ func _ready():
 	var player_character = RunData.get_player_character(0)
 	if player_character == null:
 		return
-	_saved_run = _ap_client.saved_runs_progress.get_saved_run(player_character.my_id)
+	var saved_run_raw = _ap_client.saved_runs_progress.get_saved_run(player_character.my_id)
 	
 	var loader_v2 = ProgressDataLoaderV2.new("")
-	var deserialized_run = loader_v2.deserialize_run_state(_saved_run)
-	var player_data = deserialized_run["players_data"][0]
+	_saved_run = loader_v2.deserialize_run_state(saved_run_raw)
+	var player_data = _saved_run["players_data"][0]
 	
 	RunData.players_data[0].current_level = player_data.current_level
 	for item in player_data.items:
@@ -38,8 +38,8 @@ func _ready():
 	TempStats.reset()
 	LinkedStats.reset()
 	
-	#_stats_container.disable_focus()
-	#_stats_container.update_player_stats(0)
+	_stats_container.disable_focus()
+	_stats_container.update_player_stats(0)
 	
 	_popup_manager.add_item_popup(_item_popup, 0)
 	_popup_manager.connect_inventory_container(_weapons_container)
@@ -56,4 +56,15 @@ func _ready():
 	_gold_container.update_value(player_data.gold)
 
 
-	_background.texture = ZoneService.get_zone_data(deserialized_run.current_zone).ui_background
+	_background.texture = ZoneService.get_zone_data(_saved_run.current_zone).ui_background
+
+
+func _on_NewRunButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_ResumeButton_pressed():
+	RunData.resume_from_state(_saved_run)
+	RunData.resumed_from_state_in_shop = true
+	var scene:= "res://ui/menus/shop/shop.tscn"
+	var _error = get_tree().change_scene(scene)
