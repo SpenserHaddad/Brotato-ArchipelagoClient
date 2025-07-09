1d1175a4e4fc4b595970d036fc05fe8443706c76
Spenser Haddad <spenser.haddad@gmail.com>
2025-07-08 23:15:03 -0400

    Update max wave tracking to use updated progress interface

--- client_mod/mods-unpacked/RampagingHippy-Archipelago/progress/characters.gd
+++ client_mod/mods-unpacked/RampagingHippy-Archipelago/progress/characters.gd
@@ -17,12 +17,6 @@ var _character_run_progress_slot_data_key: String
 	
 func _init(ap_client, game_state).(ap_client, game_state):
 	character_info = {}
-	for character in constants.CHARACTER_NAME_TO_ID:
-		character_info[character] = CharacterProgress.new()
-
-	var _status = _ap_client.connect(
-		"data_storage_updated", self, "_on_session_data_storage_updated"
-	)
 
 func on_item_received(item_name: String, _item):
 	if constants.CHARACTER_NAME_TO_ID.has(item_name):
@@ -42,6 +36,7 @@ func on_room_updated(updated_room_info: Dictionary):
 
 func on_connected_to_multiworld():
 	var _default_char_run_progress: Dictionary = {}
+	character_info.clear()
 
 	for character in constants.CHARACTER_NAME_TO_ID:
 		# We'll determine if characters are unlocked when the item is received, 
@@ -49,12 +44,12 @@ func on_connected_to_multiworld():
 		var character_won_loc_name = constants.RUN_COMPLETE_LOCATION_TEMPLATE.format({"char": character})
 		var character_won_loc_id = _ap_client.data_package.location_name_to_id[character_won_loc_name]
 		var character_won = character_won_loc_id in _ap_client.checked_locations
+		character_info[character] = CharacterProgress.new()
 		character_info[character].won_run = character_won
 		character_info[character].won_run_location_id = character_won_loc_id
 		# Clear unlocked flag. We'll unlock characters with on_item_received
 		character_info[character].unlocked = false
 		_default_char_run_progress[character] = {"max_wave_completed": 0}
-
 
 	_character_run_progress_slot_data_key = "%s_character_run_progress" % _ap_client.player
 	_ap_client.set_value(
@@ -74,9 +69,10 @@ func on_wave_finished(wave_number: int, character_ids: Array, _is_run_lost: bool
 	var updated_run_progress = {}
 	for character_id in character_ids:
 		var character = constants.CHARACTER_ID_TO_NAME[character_id]
-		if character in updated_run_progress:
-			# This can happen if multiple players pick the same character in co-op.
+		if character in updated_run_progress or character_info[character].max_wave_completed > wave_number:
+			# The former can happen if multiple players pick the same character in co-op.
 			continue
+			
 		updated_run_progress[character] = {"max_wave_completed": wave_number}
 
 	# We'll update the local character info when we get the SetReply
@@ -88,7 +84,7 @@ func on_wave_finished(wave_number: int, character_ids: Array, _is_run_lost: bool
 		true
 	)
 
-func _on_session_data_storage_updated(key: String, new_value, _original_value = null):
+func on_data_storage_updated(key: String, new_value, _original_value = null):
 	if key == _character_run_progress_slot_data_key:
-		for character in character_info:
-			character_info[character].max_wave_completed = new_value[character]["max_wave_completed"]
+		for updated_character in new_value:
+			character_info[updated_character].max_wave_completed = new_value[updated_character]["max_wave_completed"]
