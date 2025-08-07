51713027af5452bf35d68dd8c925882728d8fbe3
Spenser Haddad <spenser.haddad@gmail.com>
2025-08-06 22:52:27 -0400

    Bump to version 0.10.0 (#95)

--- client_mod/mods-unpacked/RampagingHippy-Archipelago/mod_main.gd
+++ client_mod/mods-unpacked/RampagingHippy-Archipelago/mod_main.gd
@@ -3,7 +3,7 @@ extends Node
 
 # Brotato Archipelago Multiworld Randomizer Client
 const MOD_NAME = "RampagingHippy-Archipelago"
-const MOD_VERSION = "0.9.1"
+const MOD_VERSION = "0.10.0"
 const LOG_NAME = MOD_NAME + "/mod_main"
 
 const ApWebSocketConnection = preload("res://mods-unpacked/RampagingHippy-Archipelago/ap/ap_websocket_connection.gd")
