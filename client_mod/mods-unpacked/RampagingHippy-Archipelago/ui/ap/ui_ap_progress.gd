7a809926c8390af9633eeadcb7cf76102d846f73
Spenser Haddad <spenser.haddad@gmail.com>
2026-04-26 19:02:47 -0400

    Remove dead code

--- client_mod/mods-unpacked/RampagingHippy-Archipelago/ui/ap/ui_ap_progress.gd
+++ client_mod/mods-unpacked/RampagingHippy-Archipelago/ui/ap/ui_ap_progress.gd
@@ -46,36 +46,3 @@ func update_shop_slots_ui():
 func update_all_crate_progress_ui():
 	_common_crates_progress_new.update_progress(_ap_client.common_loot_crate_progress, _ap_client.wins_progress.num_wins)
 	_legendary_crates_progress_new.update_progress(_ap_client.legendary_loot_crate_progress, _ap_client.wins_progress.num_wins)
-#	_update_crate_progress_ui(
-#		_common_crates_progress,
-#		_common_crates_next_group,
-#		_ap_client.common_loot_crate_progress
-#	)
-#	_update_crate_progress_ui(
-#		_legendary_crates_progress,
-#		_legendary_crates_next_group,
-#		_ap_client.legendary_loot_crate_progress
-#	)
-	
-func _update_crate_progress_ui(progress_control, groups_control, crate_progress):
-	var crate_type = tr("RHAP_%s_CRATE_LABEL" % crate_progress.crate_type.to_upper())
-	var num_checked = crate_progress.total_locations_checked()
-	var checks_available = crate_progress.num_unlocked_locations
-	var total_checks = crate_progress.total_checks
-
-	# Crate locations checked / Available/ Total
-	progress_control.set_value("%d / %d / %d" % [num_checked, checks_available, total_checks])
-	var next_group_idx = crate_progress.last_unlocked_group_idx + 1
-	if next_group_idx < crate_progress.loot_crate_groups.size():
-		var next_crate_group = crate_progress.loot_crate_groups[next_group_idx]
-		var wins_needed = next_crate_group.wins_to_unlock - _ap_client.wins_progress.num_wins
-		ModLoaderLog.debug(
-			"Updating loot %s crate progress: checked=%d, available=%d, total=%d, wins_needed=%d, wins_to_unlock=%d, num_wins=%d" % [
-				crate_type, num_checked, checks_available, total_checks, wins_needed, next_crate_group.wins_to_unlock, _ap_client.wins_progress.num_wins
-			],
-			LOG_NAME
-		)
-		var wins_str = tr("RHAP_PROGRESS_WINS_SINGULAR") if wins_needed == 1 else tr("RHAP_PROGRESS_WINS_PLURAL")
-		groups_control.set_value(wins_str)
-	else:
-		groups_control.set_value(tr("RHAP_PROGRESS_CRATES_ALL_AVAILABLE").format({crate_type=crate_type}))
